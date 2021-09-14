f05e0d7e5e28be26819d449e8ff5017ade0f3646
Felix Titov <titow.feliks@yandex.ru>
2021-09-14 16:48:29 +0300

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,11 +12,24 @@ public class Main {
 
     public static void main(String[] arr) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите математическое выражение : ");
-        String str = in.nextLine();
-        str = Calculating.StringToPolsk(str);
-        System.out.print("Польская запись : ");
-        System.out.println(str);
-        System.out.println("Результат операции : "+Calculating.exchangeToDouble(str));
+        String close = "";
+        while(!close.equals("close")) {
+            System.out.println("------------------------------------------------------------------------------------");
+            System.out.println("Если хотите завершить программу введите \"close\".\n Иначе нажмите enter");
+            close = in.nextLine();
+            if(!close.equals("close")) {
+                try {
+
+                    System.out.print("Введите математическое выражение : ");
+                    String str = in.nextLine();
+                    str = com.company.Calculating.StringToPolsk(str);
+                    System.out.print("Польская запись : ");
+                    System.out.println(str);
+                    System.out.println("Результат операции : " + com.company.Calculating.exchangeToDouble(str));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
